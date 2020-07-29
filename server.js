6bfdd38e3b6c7ad7530dba54052da39f45622a0b
Blackbird <ksilva.rod@gmail.com>
2020-07-28 20:40:47 -0500

    fixing server

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var fs = require("fs");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3001;
 
 app.use(express.static('public'))
 
